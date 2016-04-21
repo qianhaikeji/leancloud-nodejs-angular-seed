f9334ea6cac99c96a55762b352a8ea8b55aa8d4b
deng han <miracledenghan@163.com>
2016-04-21 19:37:07 +0800

    modify readme

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var app = require('./server-modules/app');
 
 // 端口一定要从环境变量 `LC_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
-var PORT = parseInt(process.env.LC_APP_PORT || 3002);
+var PORT = parseInt(process.env.LC_APP_PORT || 3000);
 app.listen(PORT, function () {
   console.log('Node app is running, port:', PORT, '\n\n\n\n\n\n');
 });
